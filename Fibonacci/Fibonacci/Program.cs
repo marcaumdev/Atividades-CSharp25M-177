2136c249819d19fcfc12190e632edbfd852c4168
marcaumdev <marcaumdev@gmail.com>
2025-08-22 11:54:09 -0300

    feat: Atividades realizada na aula do dia 22-08-2025

---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Quantos termos você deseja imprimir: ");
+int quantidadeTermos = Convert.ToInt32(Console.ReadLine());
+
+int numeroAtual = 0;
+int numeroAnterior = 1;
+int intermediaria;
+
+for(int i = 1; i <= quantidadeTermos; i++)
+{
+    Console.Write($" {numeroAtual}");
+    //guarda temporariamente o valor do numero atual
+    intermediaria = numeroAtual;
+    // calcula o novo número atual
+    numeroAtual = numeroAtual + numeroAnterior;
+    // Salva o numero anterior
+    numeroAnterior = intermediaria;
+    
+}
---
+++ QuadradoDeUmaSequencia/QuadradoDeUmaSequencia/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Informe a quantidade de número que deseja calcular: ");
+int resposta = Convert.ToInt32(Console.ReadLine());
+
+for(int numero = 1; numero <= resposta; numero++)
+{
+    Console.WriteLine($"Quadrado do número {numero}: {numero * numero}");
+}
